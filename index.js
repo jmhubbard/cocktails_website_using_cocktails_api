f33e86b3423f33a5e25e89104a9959ab1d5c8d18
Jason Hubbard <jasonhubb@gmail.com>
2020-10-20 17:20:06 -0700

    create index.js file

---
+++ index.js
@@ -0,0 +1,42 @@
+// Load the Express package as a module
+const express = require("express");
+
+const test = require("./public/js/test");
+
+// const articles = require("./quotes/articles")
+
+// Access the exported service
+const app = express();
+
+app.use(express.static('public'));
+
+// Enable CORS (see https://enable-cors.org/server_expressjs.html)
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
+// Return a string for requests to the root URL ("/")
+app.get("/", (request, response) => {
+  response.send("Hello from Express1!");
+});
+
+  // Return the articles list in JSON format
+  app.get("/api/articles", (request, response) => {
+    response.json(articles);
+  });
+
+  app.get("/quotes", (request, response) => {
+    response.sendFile(`${__dirname}/quotes.html`);
+    });
+
+// Start listening to incoming requests
+// If process.env.PORT is not defined, port number 3000 is used
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
+});
+
