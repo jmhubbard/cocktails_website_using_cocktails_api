5731d78f537abbdc22f9adf91bba86b21558bc8a
Jason Hubbard <jasonhubb@gmail.com>
2020-10-21 20:36:58 -0700

    retrieve 3 random drinks and create elemetns on page

--- public/js/test.js
+++ public/js/test.js
@@ -1,5 +1,54 @@
-const test = ()=> {
-    console.log("Test importing function")
-};
 
-test();
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    .then(response => response.json())
+    .then(randomDrink => {
+        const drink = randomDrink.drinks[0];
+        const drinkName = drink.strDrink;
+        const drinkImageURL = drink.strDrinkThumb;
+
+        const randoDrinkName1 = document.getElementById("randomDrinkName1");
+        randoDrinkName1.textContent = drinkName;
+
+        const randoDrinkImageURL1 = document.getElementById("randomDrinkImage1");
+        randoDrinkImageURL1.src = drinkImageURL;
+        
+    })
+    .catch(err => {
+        console.log(err.message);
+    });
+
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    .then(response => response.json())
+    .then(randomDrink => {
+        const drink = randomDrink.drinks[0];
+        const drinkName = drink.strDrink;
+        const drinkImageURL = drink.strDrinkThumb;
+
+        const randoDrinkName1 = document.getElementById("randomDrinkName2");
+        randoDrinkName1.textContent = drinkName;
+
+        const randoDrinkImageURL1 = document.getElementById("randomDrinkImage2");
+        randoDrinkImageURL1.src = drinkImageURL;
+        
+    })
+    .catch(err => {
+        console.log(err.message);
+    });
+
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    .then(response => response.json())
+    .then(randomDrink => {
+        const drink = randomDrink.drinks[0];
+        const drinkName = drink.strDrink;
+        const drinkImageURL = drink.strDrinkThumb;
+
+        const randoDrinkName1 = document.getElementById("randomDrinkName3");
+        randoDrinkName1.textContent = drinkName;
+
+        const randoDrinkImageURL1 = document.getElementById("randomDrinkImage3");
+        randoDrinkImageURL1.src = drinkImageURL;
+        
+    })
+    .catch(err => {
+        console.log(err.message);
+    });
