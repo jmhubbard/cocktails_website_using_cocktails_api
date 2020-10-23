3d50422b7b781a9ed0a9e9470272dd6d62fa2dd7
Jason Hubbard <jasonhubb@gmail.com>
2020-10-22 19:59:31 -0700

    delete unused geturletter.js file

--- public/js/geturlletter.js
+++
@@ -1,9 +0,0 @@
-const currentUrlDrinkLetter = () => {
-    const testelement = document.getElementById("inputtest");
-    const urlString = window.location.href.toLowerCase();
-    // urlString.toLowerCase();
-    const urlArray = urlString.split("drinksatoz/");
-    console.log(urlArray[1]);
-};
-
-currentUrlDrinkLetter();
