0b734d72cd40d365eae9effafb5d9498aec0506d
Jason Hubbard <jasonhubb@gmail.com>
2020-10-22 19:40:26 -0700

    change letter dropdown ID

--- public/js/letterdropdown.js
+++ public/js/letterdropdown.js
@@ -1,4 +1,4 @@
-const letterLinks = document.getElementById("dropdownmenu");
+const letterLinks = document.getElementById("drinksazdropdownmenu");
 
 const createLetterNav = () => {
     const navSymbols = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
